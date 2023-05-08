3df48eb5cb6120acffb428b2ee70d7680dfec198
whwjddls <red231234@gmail.com>
2023-05-08 10:09:39 +0900

    회원가입 페이지 구현

---
+++ src/models/UserStorage.js
@@ -0,0 +1,33 @@
+"use strict";
+
+class UserStorage {
+    static #users= {
+        id : ["woorimIT","나개발","김팀장"],
+        psword : ["1234", "1234","123456"],
+        name:["우리밋","나개발","김팀장"]
+    };
+    static getUsers(...fields) {
+        const users = this.#users;
+        const newUsers = fields.reduce((newUsers,field) => {
+            if (users.hasOwnProperty(field)) {
+                newUsers[field] = users[field];
+            }
+            return newUsers;
+        },{});
+        return newUsers;
+    }
+
+    static getUserInfo(id) {
+        const users = this.#users;
+        const idx = users.id.indexOf(id);
+        const usersKeys = Object.keys(users);
+        const userInfo = usersKeys.reduce((newUser, info) => {
+            newUser[info] = users[info][idx];
+            return newUser;
+        }, {});
+
+        return userInfo;
+    }
+}
+
+module.exports = UserStorage;
