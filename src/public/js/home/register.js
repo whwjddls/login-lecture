a84841c1fbb23d60f8d8a1eb40f62d9ecaf32a9d
whwjddls <red231234@gmail.com>
2023-05-08 11:21:23 +0900

    회원가입 라우팅 구현

--- src/public/js/home/register.js
+++ src/public/js/home/register.js
@@ -9,6 +9,9 @@ const registerBtn = document.querySelector("#button");
 registerBtn.addEventListener("click", register);
 
 function register() {
+    if (!id.value) return alert("아이디를 입력해주시오.")
+    if (psword != confirmPsword) return alert("비밀번호가 일치하지 않습니다.")
+
     const req = {
         id : id.value,
         name : name.value,
