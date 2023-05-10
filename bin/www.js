000f6d3599cc8fd48cdbbc17efc7e2dd91a01ac6
whwjddls <red231234@gmail.com>
2023-05-10 17:27:56 +0900

    로그 관리 모듈 winston 등록 및 설정

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,8 @@
 "use strict";
 const PORT = process.env.PORT || 3000;
 const app = require("../app");
+const logger = require("../src/config/logger")
 
 app.listen(PORT, () => {
-    console.log("서버 가동");
+    logger.info(`${PORT} 포트에서 서버가 가동되었습니다`);
 });
